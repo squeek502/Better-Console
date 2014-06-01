01b8baab109463b8901d79cc7fdbaf26b850d2e1
squeek <squeek502@hotmail.com>
2014-05-31 21:59:26 -0700

    Bumped API version to 6

--- modinfo.lua
+++ modinfo.lua
@@ -12,7 +12,7 @@ version = "1.3.0"
 forumthread = "/files/file/250-better-console/"
 
 -- This lets other players know if your mod is out of date, update it to match the current version in the game
-api_version = 5
+api_version = 6
 
 -- Can specify a custom icon for this mod!
 icon_atlas = "modicon.xml"
